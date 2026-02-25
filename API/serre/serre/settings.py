0dff0c082390f03269b77d57c05a04a7effbe833
raspberry <serre.connecter12@gmail.com>
2026-02-25 09:55:39 +0100

    css fonctionne

--- API/serre/serre/settings.py
+++ API/serre/serre/settings.py
@@ -116,4 +116,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Use an absolute static URL so generated paths start with `/static/`
+STATIC_URL = '/static/'
