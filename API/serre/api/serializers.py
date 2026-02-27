bcfd03ad8eba19923b619a6c1b5aa7c7ca34b868
EthanStasse <ethan.stasse88@gmail.com>
2026-02-27 17:03:03 +0100

    new beginning

--- API/serre/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Serre
-
-class SerreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Serre
-        fields = '__all__'
--- API/serre/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('serre/', views.get_serre),
-    path('last/', views.last_serre),
-    path('toit/', views.toit_cmd),
-]
