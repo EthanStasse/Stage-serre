a2ceb2071519c0d17a603a0ff1e2928ea8af748c
EthanStasse <ethan.stasse88@gmail.com>
2026-03-02 15:15:25 +0100

    login

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
     path('api/serre/', views.get_serre),
     path('api/last/', views.last_serre),
     path('api/toit/', views.toit_cmd),
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -31,6 +31,7 @@ def login(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password") 
+    return render(request, "login.html")
  
 
 def index(request):
